ee74e61ff7567a0fd4e5a5ad30f7411a97ad876e
Muhammed Salih Guler <gueler@karlmax-berlin.com>
2017-03-15 18:30:23 +0100

    Initial commit

---
+++ app/src/main/java/com/iamsalih/uq_app/LandingActivity.java
@@ -0,0 +1,18 @@
+package com.iamsalih.uq_app;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+/**
+ * Created by muhammedsalihguler on 15/03/2017.
+ */
+
+public class LandingActivity extends Activity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_landing_view);
+    }
+}
